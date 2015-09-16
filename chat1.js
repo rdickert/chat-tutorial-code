f5ebf58f6db3b5601be1f506a36443be943aead2
Robert Dickert <robert.dickert@gmail.com>
2015-09-16 15:45:52 -0600

    Part Two complete

---
+++ chat1.js
@@ -0,0 +1,51 @@
+Messages = new Mongo.Collection("msgs");
+
+Meteor.methods({
+  sendMessage: function (messageText) {
+    /* authorization code */
+
+    Messages.insert({
+      messageText: messageText,
+      createdAt: new Date(),
+      username: "anonymous"
+    });
+  }
+});
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("messages", function () {
+    return Messages.find({}, {sort: {createdAt: -1}, limit: 5});
+  });
+}
+
+/* scrolling code */
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("messages");
+
+  Template.body.helpers({
+    recentMessages: function () {
+      return Messages.find({}, {sort: {createdAt: 1}});
+    },
+    /* unread message helper */
+  });
+
+  /*chat window scrolling*/
+
+  Template.body.events({
+    "submit .new-message": function (event) {
+      var text = event.target.text.value;
+
+      Meteor.call("sendMessage", text);
+
+      event.target.text.value = "";
+      event.preventDefault();
+    },
+
+    /* scroll event */
+
+  });
+  /*account config*/
+}
