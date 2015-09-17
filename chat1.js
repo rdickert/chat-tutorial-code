0d47cc43b9094597b08de50c21279eb12d371d88
Robert Dickert <robert.dickert@gmail.com>
2015-09-17 13:38:07 -0600

    Part Four complete

--- chat1.js
+++ chat1.js
@@ -17,37 +17,73 @@ Meteor.methods({
 if (Meteor.isServer) {
   // This code only runs on the server
   Meteor.publish("messages", function () {
-    return Messages.find({}, {sort: {createdAt: -1}, limit: 5});
+    return Messages.find();
   });
 }
 
-/* scrolling code */
+var autoScrollingIsActive = false;
+var thereAreUnreadMessages = new ReactiveVar(false);
+scrollToBottom = function scrollToBottom (duration) {
+  var messageWindow = $(".message-window");
+  var scrollHeight = messageWindow.prop("scrollHeight");
+  messageWindow.stop().animate({scrollTop: scrollHeight}, duration || 0);
+};
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Meteor.subscribe("messages");
+  Meteor.subscribe("messages", {
+    onReady: function () {
+      scrollToBottom();
+      autoScrollingIsActive = true;
+    }
+  });
 
   Template.body.helpers({
     recentMessages: function () {
       return Messages.find({}, {sort: {createdAt: 1}});
     },
-    /* unread message helper */
+    thereAreUnreadMessages: function () {
+      return thereAreUnreadMessages.get();
+    }
   });
 
-  /*chat window scrolling*/
+  Template.message.onRendered(function () {
+    if (autoScrollingIsActive) {
+      scrollToBottom(250);
+    } else {
+      if (Meteor.user() && this.data.username !== Meteor.user().username) {
+        thereAreUnreadMessages.set(true);
+      }
+    }
+  });
 
   Template.body.events({
     "submit .new-message": function (event) {
       var text = event.target.text.value;
 
       Meteor.call("sendMessage", text);
+      scrollToBottom(250);
 
       event.target.text.value = "";
       event.preventDefault();
     },
 
-    /* scroll event */
+    "scroll .message-window": function () {
+      var howClose = 80;  // # pixels leeway to be considered "at Bottom"
+      var messageWindow = $(".message-window");
+      var scrollHeight = messageWindow.prop("scrollHeight");
+      var scrollBottom = messageWindow.prop("scrollTop") + messageWindow.height();
+      var atBottom = scrollBottom > (scrollHeight - howClose);
+      autoScrollingIsActive = atBottom ? true : false;
+      if (atBottom) {        // <--new
+        thereAreUnreadMessages.set(false);
+      }
+    },
 
+    "click .more-messages": function () {
+      scrollToBottom(500);
+      thereAreUnreadMessages.set(false);
+    }
   });
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
